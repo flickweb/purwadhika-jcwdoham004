05568302a32e8d7c2438f207f9bcc60bb2fbae7a
flickweb <felixrumambi@gmail.com>
2025-09-02 19:20:03 +0700

    session03 exercise done

--- module-01/session-03/index.js
+++ module-01/session-03/index.js
@@ -123,23 +123,173 @@ const rl = readline.createInterface({
 
 
 // Write a code to capitalize the first letter of each word in a string
-let words;
+// let words;
 
-rl.question("Input your word/words here : ", (answer7)=>{
-    words = answer7.split(" ");
-    for(let i = 0; i < words.length; i++){
-        let wordsCaps = words[i].charAt(0).toUpperCase();
-        let wordsRest = words[i].slice(1);
+// rl.question("Input your word/words here : ", (answer7)=>{
+//     words = answer7.split(" ");
+//     for(let i = 0; i < words.length; i++){
+//         let wordsCaps = words[i].charAt(0).toUpperCase();
+//         let wordsRest = words[i].slice(1);
         
-        //ini array
-        words[i] = wordsCaps + wordsRest;
-    }
-    console.log(words.join(" "));
-    rl.close();
-})
+//         //ini array
+//         words[i] = wordsCaps + wordsRest;
+//     }
+//     console.log(words.join(" "));
+//     rl.close();
+// })
+
 
 
 // Write a code to swap the case of each character from string
 // Example : ‘The QuiCk BrOwN Fox’ -> ‘ tHE qUIcK bRoWn fOX’
 
-//test
+// let words2;
+// let words2Split;
+// let words2New = [];
+
+// rl.question("Input your word/words here : ", (answer8)=>{
+//     words2 = answer8.split(" ");
+//     // console.log(words2);
+//     words2Split = answer8.split("");
+//     // console.log(words2Split)
+//     // console.log(words2Split.join(""))
+
+//     for(let i = 0; i < words2Split.length; i++){
+//         // console.log(i);
+//         if(words2Split[i] === words2Split[i].toUpperCase()){
+//             // console.log("uppercase!!")
+//             words2New.push(words2Split[i].toLowerCase());
+//         }else{
+//             // console.log("no no no!");
+//             words2New.push(words2Split[i].toUpperCase());
+//         }
+//     }
+
+//     console.log(words2New.join(""));
+
+//     rl.close();
+// })
+
+
+// Write a code to find the largest of two given integers
+// Example : num1 = 42, num2 = 27 → 42
+
+// let inputNumber1;
+// let inputNumber2;
+
+// rl.question("Input the first number : ",(answer9)=>{
+//     inputNumber1 = parseInt(answer9);
+//     // console.log("test");
+//     rl.question("Input the second number : ",(answer10)=>{
+//         inputNumber2 = parseInt(answer10);
+//         if(inputNumber1 > inputNumber2){
+//             console.log(inputNumber1);
+//         }else if(inputNumber2 > inputNumber1){
+//             console.log(inputNumber2);
+//         }else{
+//             console.log("First and second number is equal to each other.")
+//         }
+//         rl.close();
+//     })
+//     // console.log("test2");
+// })
+
+
+// Write a conditional statement to sort three numbers
+// Example : num1 = 42, num2 = 27, num3 = 18 → 18, 27, 42
+
+// let numInput1;
+// let numInput2;
+// let numInput3;
+// let temp = 0; // jangan lupa kasi value dulu, kalo ga undefined like before
+
+// rl.question("Enter the first number : ", (answer11) => {
+//     numInput1 = Number(answer11);
+//     rl.question("Enter the second number : ", (answer12) => {
+//         numInput2 = Number(answer12);
+//         rl.question("Enter the third number : ", (answer13) => {
+//             numInput3 = Number(answer13);// use Number() karna what you type is considered a string, not a number. Jangan lupa, awalnya km lupa
+
+//             if(numInput1 > numInput2){
+//                 temp = numInput1; // kamu masukin reversed awalnya, you have to store the old file first dumdum
+//                 numInput1 = numInput2;
+//                 numInput2 = temp;
+//             }
+//             if(numInput1 > numInput3){
+//                 temp = numInput1; 
+//                 numInput1 = numInput3;
+//                 numInput3 = temp;
+//             }
+//             if(numInput2 > numInput3){
+//                 temp = numInput2;
+//                 numInput2 = numInput3;
+//                 numInput3 = temp;
+//             }
+
+//             // you used else if instead of 3 ifs. with else ifs, nanti programmu runs only one branch and skips the rest.
+//             // kalo pake 3 ifs, dia check everything independently. that way other conditions still get compared and swapped if needed.
+
+
+//             console.log(`${numInput1} , ${numInput2}, ${numInput3}`);
+
+            
+//             rl.close()
+//         })
+//     })
+// })
+
+
+
+// Write a code that shows 1 if the input is a string, 2 if the input is a number, and 3 for others datatype.
+// Example : “hello” → 1
+
+
+// try to find an answer to this.. error kalo ak taruh ! or @ (yes bukan data type but worth a shot).
+// paling pake try-cactch?
+// rl.question("Type an input here : ",(inputDetect) => {
+
+//     let inputDetectType = JSON.parse(inputDetect); // ini tries to parse into a real type
+
+//     if (typeof inputDetectType === "number" && !isNaN(inputDetectType)/*isNaN itu buat check if its a number or not*/) {
+//         console.log("2");
+//       } else if (typeof inputDetectType === "string") {
+//         console.log("1");
+//       } else {
+//         console.log("3");
+//       }
+//       rl.close();
+// })
+
+
+
+// easy way, cuman tinggal masukin let nya dikasih data.
+
+// let inputDetect = [];
+
+// if (typeof inputDetect === "string") {
+//     console.log("1");
+//   } else if (typeof inputDetect === "number") {
+//     console.log("2");
+//   } else {
+//     console.log("3");
+//   }
+  
+
+
+// Write a code to change every letter a into * from a string of input
+// Example : ‘An apple a day keeps the doctor away’ -> `*n *pple * d*y keeps the doctor *w*y`
+
+// rl.question("Type a sentence: ", (inputChangeA) => {
+//     let resultForA = "";
+  
+//     for (let i = 0; i < inputChangeA.length; i++) {
+//       if (inputChangeA[i] === "a" || inputChangeA[i] === "A") {
+//         resultForA += "*";
+//       } else {
+//         resultForA += inputChangeA[i];
+//       }
+//     }
+  
+//     console.log(resultForA);
+//     rl.close();
+//   });
