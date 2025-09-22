e9a012a20943f1a7fc1942bde1627e189a638b16
flickweb <felixrumambi@gmail.com>
2025-09-22 21:38:08 +0700

    feat session-03 done, font unready

--- module-02/session-04/my-app/src/app/layout.tsx
+++ module-02/session-04/my-app/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Josefin_Sans } from "next/font/google";
+
 import "./globals.css";
 import ThemesProvider from "@/components/providers/themeProvider";
 
@@ -11,6 +12,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const josefinSans = Josefin_Sans({
+  variable: "--font-jose-sans",
+  subsets:["latin"],
 });
 
 export const metadata: Metadata = {
@@ -26,7 +32,7 @@ export default function RootLayout({
   return (
 <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${josefinSans.variable} antialiased`}
       >
         <ThemesProvider>{children}</ThemesProvider>
       </body>
--- module-02/session-04/my-app/src/app/page.tsx
+++ module-02/session-04/my-app/src/app/page.tsx
@@ -2,20 +2,11 @@
 import { useTheme } from "next-themes";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center font-jose">
       <div className="h-[300px] w-[1440px] fixed z-0">
         <img src={"/BitmapDark.jpg"} alt="dark background pic" />
       </div>
@@ -26,39 +17,47 @@ export default function Home() {
             <p className="text-[40px] tracking-[15px] font-bold m-0 text-white">
               TODO
             </p>
-            <img src={"/moon.svg"}></img>
+            <button
+              onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            >
+              {theme === "light" ? (
+                <img src={"/sun.svg"}></img>
+              ) : (
+                <img src={"/moon.svg"}></img>
+              )}
+            </button>
           </div>
           <div className="flex flex-col gap-6">
-            <div className="flex flex-row items-center bg-[#ffffff] border-b border-black h-[64px] w-[100%] gap-5 px-4 text-[18px] rounded-md">
+            <div className="flex flex-row items-center bg-[#ffffff] border-b border-black h-[64px] w-[100%] gap-5 px-4 text-[18px] rounded-md dark:bg-[#171823] dark:border-[#393A4B]">
               <Checkbox id="newCode" />
               <Label htmlFor="newCode">Create a new todo..</Label>
             </div>
             <div className="flex flex-col bg-[#ffffff] h-[439px] w-[100%] rounded-md drop-shadow-xl">
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px]">
+              <div className="flex flex-row items-center basis-1/7 border-b border-black gap-5 px-4 text-[18px] dark:bg-[#171823] dark:border-[#393A4B]">
                 <Checkbox id="newCode" />
                 <Label htmlFor="newCode">Create a new todo..</Label>
               </div>
-              <div className="flex flex-row items-center justify-between basis-1/7 px-4 text-[14px] text-[#9495A5]">
+              <div className="flex flex-row items-center justify-between basis-1/7 px-4 text-[14px] text-[#9495A5]  dark:bg-[#171823] dark:border-[#393A4B] dark:text-[#5B5E7E]">
                 <div>5 items left</div>
                 <div className="flex flex-row gap-2">
                   <span>All</span>
